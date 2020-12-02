cc332b78ea36959bd57285979143e04f710e3201
= <=>
2020-12-02 10:10:51 +0200

    Modified files. Test main now tests all files, mock api has been extended with a patch function for events, test auth formatted better

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,6 +7,7 @@ from tests import mock_api
 from unittest.mock import patch
 from authentication.authenticate import *
 
+
 class TestAuthentication(unittest.TestCase):
 
     def test_open_create_credits_file_file_exists(self):
@@ -26,7 +27,7 @@ class TestAuthentication(unittest.TestCase):
             open_create_credits_file()
             self.assertTrue(os.path.isfile(f'{user_home}/{file_name}'))
             self.assertTrue(os.path.getsize(f'{user_home}/{file_name}') > 0)
-        
+
         sys.stdout = temp
 
     def test_open_create_credits_file_empty_file_filled(self):
@@ -37,7 +38,7 @@ class TestAuthentication(unittest.TestCase):
 
         if (os.path.isfile(f'{user_home}/{file_name}')):
             os.remove(f"{user_home}/.credentials.pkl") # if the file exists, remove it
-        
+
         with open(f"{user_home}/.credentials.pkl", "w"):
             pass # open and close file to leave it empty
 
@@ -58,19 +59,16 @@ class TestAuthentication(unittest.TestCase):
         with patch('authentication.authenticate.get_flow') as mock_get_flow:
             mock_get_flow.return_value = mock_api.Flow()
             open_create_credits_file(delete_file=True)
-            
+
             file_obj = open(f'{user_home}/{file_name}', 'rb')
             saved_credentials = pickle.load(file_obj)
             file_obj.close()
             self.assertEqual(saved_credentials, mock_api.Flow().run_local_server())
-        
+
         sys.stdout = temp
 
     def test_open_create_credits_file_return(self):
         sys.stdout, temp = StringIO(), sys.stdout
-
-        user_home = os.path.expanduser('~')
-        file_name = '.credentials.pkl'
 
         with patch('authentication.authenticate.get_flow') as mock_get_flow:
             mock_get_flow.return_value = mock_api.Flow()
@@ -78,9 +76,9 @@ class TestAuthentication(unittest.TestCase):
 
             self.assertEqual(returned, mock_api.Flow().run_local_server())
             self.assertTrue(type(returned) == dict)
-        
+
         sys.stdout = temp
-    
+
     def test_open_create_credits_file_parameter(self):
         self.assertRaises(TypeError, open_create_credits_file, 'K')
         self.assertRaises(TypeError, open_create_credits_file, 0)
@@ -102,6 +100,8 @@ class TestAuthentication(unittest.TestCase):
 
     @patch('authentication.authenticate.build')
     def test_authenticate_user_invalid_credentials(self, mock_build):
+        sys.stdout, temp = StringIO(), sys.stdout
+
         with patch('authentication.authenticate.open_create_credits_file') as mock_OCF:
             mock_OCF.return_value = mock_api.Flow().run_local_server()
             mock_build.side_effect = [TypeError, TypeError]
@@ -112,6 +112,7 @@ class TestAuthentication(unittest.TestCase):
             mock_build.side_effect = [TypeError, mock_api.Mock_Service()]
             service_returned = authenticate_user({'Test Number': 4, 'Test Number': 5})
             self.assertEqual(type(service_returned), type(mock_api.Mock_Service()))
+        sys.stdout = temp
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,56 +1,62 @@
 import unittest
-from io import StringIO
+# from io import StringIO
 import sys
 from test_base import run_unittests
-from test_base import captured_io
-import os
-import pickle
-import csv
-from data import create_data
-from data import display_calendar
-import app
-from unittest.mock import patch
-from authentication.authenticate import *
-from io import StringIO
-import re
-from tabulate import tabulate
+# from test_base import captured_io
+# import os
+# import pickle
+# import csv
+# from data import create_data
+# from data import display_calendar
+# import app
+# from unittest.mock import patch
+# from authentication.authenticate import *
+# from io import StringIO
+# import re
+# from tabulate import tabulate
 
 
 class test_acceptance(unittest.TestCase):
 
-    
     def test_unittest_app_exist(self):
-        import tests.test_app 
-        self.assertTrue('test.test_app' in sys.modules, "test_app module should be found")
-
+        import tests.test_app
+        self.assertTrue('tests.test_app' in sys.modules, "test_app module should be found")
 
     def test_unittest_app_succeeds(self):
-        import tests.test_app
-        test_result = run_unittests("test_app")
-        self.assertTrue(test_result.wasSuccessful(), "unit tests should succeed")
-
+        test_result = run_unittests("tests.test_app")
+        self.assertTrue(test_result.wasSuccessful(), "app unit tests should succeed")
 
     def test_unittest_auth_exist(self):
         import tests.test_auth
-        self.assertTrue('test.test_auth' in sys.modules, "test_auth module should be found")
-
+        self.assertTrue('tests.test_auth' in sys.modules, "test_auth module should be found")
 
     def test_unittest_auth_succeeds(self):
-        import tests.test_auth
-        test_result = run_unittests("test_auth")
-        self.assertTrue(test_result.wasSuccessful(), "unit tests should succeed")
-
+        test_result = run_unittests("tests.test_auth")
+        self.assertTrue(test_result.wasSuccessful(), "auth unit tests should succeed")
 
     def test_unittest_display_exist(self):
         import tests.test_display
-        self.assertTrue('test_display' in sys.modules, "test_display module should be found")
-
+        self.assertTrue('tests.test_display' in sys.modules, "test_display module should be found")
 
     def test_unittest_display_succeeds(self):
-        import tests.test_display
-        test_result = run_unittests("test_display")
-        self.assertTrue(test_result.wasSuccessful(), "unit tests should succeed")
+        test_result = run_unittests("tests.test_display")
+        self.assertTrue(test_result.wasSuccessful(), "display unit tests should succeed")
 
+    def test_unittest_test_patient_slot_booking_exists(self):
+        import tests.test_patient_slot_booking
+        self.assertTrue("tests.test_patient_slot_booking" in sys.modules, "test_patient_slot_booking module should be found.")
+
+    def test_unittest_test_patient_slot_booking_succeeds(self):
+        test_result = run_unittests("tests.test_patient_slot_booking")
+        self.assertTrue(test_result.wasSuccessful(), "patient slot booking unit tests should succeed")
+
+    def test_unittest_test_create_data_exists(self):
+        import tests.test_create_data
+        self.assertTrue("tests.test_create_data" in sys.modules, "test_create_data module should be found.")
+
+    def test_unittest_test_create_data_succeeds(self):
+        test_result = run_unittests("tests.test_create_data")
+        self.assertTrue(test_result.wasSuccessful(), "create data unit tests should succeed")
 
 
 if __name__ == '__main__':
