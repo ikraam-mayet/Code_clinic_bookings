fb51cbfb1d69d347c80cb46bd79385d13a5dd6fd
= <=>
2020-12-07 09:51:03 +0200

    Added Musa's changed booking system

--- data/volunteer/making_a_slot.py
+++ data/volunteer/making_a_slot.py
@@ -2,12 +2,15 @@ from datetime import datetime, timedelta, date
 
 
 def ask_for_date():
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+    'August', 'September', 'October', 'November', 'December']
     while True:
         try :
             date_ = input('Please enter a date i.e 07 November 2020: ').split()
         except KeyError:
             continue
-        if len(date_) != 3:
+        date_[1] = date_[1][0].upper() + date_[1][1:].lower()
+        if len(date_) != 3 or len(date_[0]) != 2 or not date_[1] in months or len(date_[2]) != 4:
             continue
         else:
             break
@@ -18,22 +21,23 @@ def ask_for_time():
 
     while True:
         time_ = input("please enter time i.e 11:30: ")
-        if len(time_) == 5:
-            return time_
+        if len(time_) == 5 and ':' in time_:
+            time_ = time_.split(':', 1)
+            if len(time_[0]) == 2  and len(time_[1]) == 2:
+                return ':'.join(time_)
         else: continue
 
 
 def call_date_time_check(days_stored):
     while True:
-        
         date_ = ask_for_date()
 
         time_ = ask_for_time()
 
         year, month, day = int(date_[2]), datetime.strptime(date_[1], '%B').month, int(date_[0])
         start_date = datetime(year, month, day, hour=int(time_.split(':')[0]), minute=int(time_.split(':')[1]))
-        end_date = start_date + timedelta(minutes=90)
-
+        end_date = start_date + timedelta(minutes=30)
+        # print(start_date.day, days_stored)
         if (start_date.day - date.today().day > days_stored):
             print("Out of range. Please select a slot within the stored days.")
             continue
@@ -47,18 +51,16 @@ def check_available_slots(booked_slots, days_stored):
 
         start_time = start_date.time()
         end_time = end_date.time()
-        
+        # print(booked_slots, str(start_time))
         for booked_slot in booked_slots[' '.join(date_)]:
-
-            starts_in_booked_slot = (start_time >= booked_slot[0] and start_time < booked_slot[1]) # bool. True if start time is in an already booked slot
-            ends_in_booked_slot = (end_time >= booked_slot[0] and end_time < booked_slot[1]) # bool. True if end time is in an already booked slot
-            if starts_in_booked_slot or ends_in_booked_slot:
+            starts_in_booked_slot = (start_time >= booked_slot[0] and end_time <= booked_slot[1]) 
+            if starts_in_booked_slot:
                 print("Slot booked. Try another date/time.")
-                continue
+                return check_available_slots( booked_slots,days_stored)
         return volunteer(start_date)
     
 
-def create_event(start_date,end_date, summary, description, student_email):
+def create_event(start_date,end_date, summary, description):
     """
     Book a time at the date and time given.
     Generate an event between the start and end date.
@@ -77,7 +79,7 @@ def create_event(start_date,end_date, summary, description, student_email):
             'timeZone': 'Africa/Johannesburg',
             },
         'attendees': [
-                {'email' : student_email}
+                {'email' : 'group2codeclinic@gmail.com'}
         ],
         'params' : {
             'sendNotifications' : True
@@ -95,26 +97,12 @@ def create_event(start_date,end_date, summary, description, student_email):
 
 def volunteer(start_date):
     summary = input("Username: ")
-    student_email = f"{summary}@student.wethinkcode.co.za"
     description = input("What topic do you want to volunteer for: ")
-    # max_time = timedelta(minutes=30)
     event_slots = list()
 
-    # start = start_time
-    # start = str(date_[0]) + ' '+ str(date_[1]) + ' '+ str(date_[2]) + ' '+str(start_time)
-
     for i in range(3):
-        # start = datetime.strptime(str(start), '%Y-%m-%dT%H:%M:%S')
-        # start = str(start)
-        # start = start[11:]
-        # end = datetime.strptime(str(start), '%Y-%m-%dT%H:%M:%S') + timedelta(minutes = 30)
-        # end = str(end)
-        # end = end[11:]
         end_date = start_date + timedelta(minutes=30)
-        events = create_event(start_date, end_date, summary, description,student_email)
+        events = create_event(start_date, end_date, summary, description)
         event_slots.append(events)
-        start_date = end_date
-        # start = datetime.strptime(str(start), '%Y-%m-%dT%H:%M:%S') + timedelta(minutes = 30)
-        
+        start_date = end_date    
     return event_slots
-
