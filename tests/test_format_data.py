94670f55eb5a8f584659fe47de8984a44c320ed7
ikraam-mayet <imayet@student.wethinkcode.co.za>
2020-12-13 22:55:13 +0200

    added unittest for format_data

--- tests/test_format_data.py
+++ tests/test_format_data.py
@@ -7,4 +7,8 @@ from unittest.mock import patch
 from data import format_data
 
 class test_format_data(unittest.TestCase):
-    pass
+
+    def test_get_time_date(self):
+        lfm = '2020-12-14T20:10:50.999'
+        result = format_data.get_time_date(lfm)
+        self.assertEqual(result, ('14 December 2020', '20:10', datetime.datetime(2020, 12, 14, 20, 10, 50, 999000)))
