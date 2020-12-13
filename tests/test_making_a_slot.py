2690fba5a1d8c4b2ccefcd3aaa9f7e5489c9f1ca
ikraam-mayet <imayet@student.wethinkcode.co.za>
2020-12-14 00:14:10 +0200

    unittest for making a slot added

--- tests/test_making_a_slot.py
+++ tests/test_making_a_slot.py
@@ -10,6 +10,8 @@ class test_making_a_slot(unittest.TestCase):
 
     expected_date = datetime.date.today()
     today_ = expected_date.strftime('%d %B %Y')
+    time_ = '11:30'
+
 
     @patch('sys.stdin', StringIO(f"{today_}\n"))
     def test_ask_for_date(self):
@@ -20,13 +22,29 @@ class test_making_a_slot(unittest.TestCase):
         sys.stdout = temp
 
 
-    @patch('sys.stdin', StringIO(f"11:30\n"))
+    @patch('sys.stdin', StringIO(f"{time_}\n"))
     def test_ask_for_time(self):
         sys.stdout, temp = StringIO(), sys.stdout
         result = making_a_slot.ask_for_time()
-        self.assertEqual(result,'11:30')
+        self.assertEqual(result,self.time_)
         sys.stdout = temp
 
 
+    @patch('sys.stdin', StringIO(f"{today_}\n{time_}\n"))
+    def test_call_date_time_check(self):
+        sys.stdout, temp = StringIO(), sys.stdout
+        result = making_a_slot.call_date_time_check(int(7))
+        answer = self.today_.split(" ")
+        self.maxDiff = None 
+        year, month, day = int(answer[2]), datetime.datetime.strptime(answer[1], '%B').month, int(answer[0])
+        start_date = datetime.datetime(year, month, day, hour=int(self.time_.split(':')[0]), minute=int(self.time_.split(':')[1]))
+        end_date = start_date + datetime.timedelta(minutes=30)
+        self.assertEqual(result, (answer, start_date, end_date))
+        sys.stdout = temp
+
+
+    @patch('sys.stdin', StringIO(f"{today_}\n11:30\n"))
     def test_check_available_slots(self): #booked_slots, days_stored needed
-        pass
+        sys.stdout, temp = StringIO(), sys.stdout
+        # result = making_a_slot.check_available_slots()
+        sys.stdout = temp
